ada0edd0a2637790c607c82461b104e9c86b1a8d
mkudryavtsev <kudryavtsev.maxim@icloud.com>
2020-02-15 17:55:26 +0300

    Testing

---
+++ src/test/java/service/SkillServiceTest.java
@@ -0,0 +1,33 @@
+package service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SkillServiceTest {
+
+    @Test
+    public void createSkill() {
+
+    }
+
+    @Test
+    public void showSkills() {
+    }
+
+    @Test
+    public void deleteSkill() {
+    }
+
+    @Test
+    public void validateSkillName() {
+    }
+
+    @Test
+    public void isExist() {
+    }
+
+    @Test
+    public void testIsExist() {
+    }
+}
